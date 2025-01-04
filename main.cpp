1ed4a82facb9eef69beb83e41fc11920cb782a58
Sxnni04 <din.alomerovic@gmail.com>
2025-01-04 14:33:10 +0100

    search for muele funktion

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 // Jeder Spieler besitzt zu Beginn 9 Spielsteine
 const int MAX_PIECES = 9;
+
+enum Symbols {
+    Player1 = 'X',
+    Player2 = 'O'
+};
+
+const std::vector<std::vector<std::string>> muehlen = {
+    {"A1", "D1", "G1"}, {"B2", "D2", "F2"}, {"C3", "D3", "E3"},
+    {"A4", "B4", "C4"}, {"E4", "F4", "G4"}, {"C5", "D5", "E5"},
+    {"B6", "D6", "F6"}, {"A7", "D7", "G7"}, {"A1", "A4", "A7"},
+    {"G1", "G4", "G7"}, {"B2", "B4", "B6"}, {"C3", "C4", "C5"}, 
+    {"D1", "D2", "D3"}, {"D5", "D6", "D7"}, {"E3", "E4", "E5"}, 
+    {"F2", "F4", "F6"}
+};
 
 class Player {
     std::string name;
@@ -13,18 +28,16 @@ class Player {
     bool placePiece(std::string position);
 
     public:
+
     Player(char playerSymbol) {
         symbol = playerSymbol;
         std::cin >> name;
     }
 
     int getpiecesPlaced() {return piecesPlaced;}
-
-    void decreasePieces() {
-        this->piecesLeft--;
-    }
-
+    void decreasePieces() {this->piecesLeft--;}
     void makeMove();
+    bool validateMuehle();
 };
 
 std::map<std::string, char> field = {
@@ -92,14 +105,47 @@ void Player::makeMove()
         choice1 = getInput();
     }
     showField();
+    validateMuehle();
+}
+
+bool Player::validateMuehle()
+{
+    for (std::vector element : muehlen) {
+        if (field[element[0]] == symbol && field[element[1]] == symbol && field[element[2]] == symbol) {
+            std::cout << "Spieler " << name << " scored a Mühle!" << std::endl
+            << "Which piece do you want to remove?" << std::endl;
+
+            std::string piece;
+            std::cin >> piece;
+
+            char otherPlayer;
+            
+            if (symbol == Player1) {
+                otherPlayer = Player2;
+            } else {
+                otherPlayer = Player1;
+            }
+
+            while (!validateInput(piece) || field[piece] != otherPlayer) {
+                std::cout << "Your choice is not possible please try again: " << std::endl;
+                std::cin >> piece;
+            }
+
+            field[piece] = '.';
+            // To Do: piecesLeft des anderen Spielers um 1 verringern
+
+            return true;
+        }
+    }
+    return false;
 }
 
 int main() {
     std::cout << "Enter Name for Player 1: " << std::endl;
-    Player p1('X');
+    Player p1(Player1);
 
     std::cout << "Enter Name for Player 2: " << std::endl;
-    Player p2('O');
+    Player p2(Player2);
 
     showField();
 
@@ -110,6 +156,4 @@ int main() {
         std::cout << std::endl;
         p2.makeMove();
     } 
-
-    // TO DO: Prüfen ob eine Mühle erzielt wurde!
 }
