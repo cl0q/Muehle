638347a52ac02033fa026274d6a66c11850f0a28
cl0q <oliver@ilczuk.com>
2025-01-14 19:24:53 +0100

    manual merge 1

---
+++ src/Player/Player.cpp
@@ -0,0 +1,17 @@
+#include "Player.h"
+
+int Player::place_stone()
+{
+    stonesPlayed++;
+    return stonesPlayed;
+}
+
+int Player::getTotalStones()
+{
+    return totalStones;
+}
+
+int Player::getStonesPlayed()
+{
+    return stonesPlayed;
+}
